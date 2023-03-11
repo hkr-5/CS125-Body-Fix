6d3692b02ec95d4e282d583586ad7aa612f06c5b
Panzerchong <panzerchong@gmail.com>
2023-03-11 12:04:21 -0800

    Update tab3.page.ts

--- app/tab3/tab3.page.ts
+++ app/tab3/tab3.page.ts
@@ -2,11 +2,27 @@ import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import axios from 'axios';
 
+const API_KEY = "ANqL8DWNmjbGyOciXVpmjB6x3hLHLW8EyzUS8z5hSZSSHjS2E7vzhlJsXQYVqiSfSQJzfW3ZeFP5SCV-jggrnLwP7xkrX47lHHn1lGEK9OdEYNKIT7_ujom13aYKZHYx";
+
+const BASE_URL = "https://api.yelp.com/v3";
+
+const SEARCH_ENDPOINT = "/businesses/search";
+
+const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
+
+interface YelpBusiness {
+  name: string;
+  rating: number;
+  phone: string;
+}
+
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
+
+
 
 export class Tab3Page {
   
@@ -87,52 +103,73 @@ export class Tab3Page {
     await star.present();
   }
   
-  // getFile(){
-  //   const options = {
-  //   method: 'GET',
-  //   headers: {
-  //     accept: 'application/json',
-  //     Authorization: 'Bearer ANqL8DWNmjbGyOciXVpmjB6x3hLHLW8EyzUS8z5hSZSSHjS2E7vzhlJsXQYVqiSfSQJzfW3ZeFP5SCV-jggrnLwP7xkrX47lHHn1lGEK9OdEYNKIT7_ujom13aYKZHYx',
-  //     'Access-Control-Allow-Origin': 'https://localhost:8100'
-  //   }
-  //     };
-  //     fetch('https://api.yelp.com/v3/businesses/search?location=irvine&term=innout&sort_by=best_match&limit=20', options)
-  //       .then(response => response.json())
-  //       .then(response => console.log(response))
-  //       .catch(err => console.error(err));
-  // }
+  getFile(){
+    const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer ANqL8DWNmjbGyOciXVpmjB6x3hLHLW8EyzUS8z5hSZSSHjS2E7vzhlJsXQYVqiSfSQJzfW3ZeFP5SCV-jggrnLwP7xkrX47lHHn1lGEK9OdEYNKIT7_ujom13aYKZHYx',
+    }
+      };
+      fetch('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=irvine&term=innout&sort_by=best_match&limit=20', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+        
+  }
+
 
  
 
 
 
-  getFile(){
-    const url = 'https://api.yelp.com/v3/businesses/search';
+//   getFile(){
+//     const url = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search';
 
-// Set up the request parameters, such as the search term and location
-const params = {
-  term: 'pizza',
-  location: 'San Francisco',
-};
+// // Set up the request parameters, such as the search term and location
+// const params = {
+//   term: 'innout',
+//   location: 'irvine',
+// };
 
-// Set up the request headers, including the API key and the CORS header
-const headers = {
-  Authorization: `Bearer ANqL8DWNmjbGyOciXVpmjB6x3hLHLW8EyzUS8z5hSZSSHjS2E7vzhlJsXQYVqiSfSQJzfW3ZeFP5SCV-jggrnLwP7xkrX47lHHn1lGEK9OdEYNKIT7_ujom13aYKZHYx`,
-  'Access-Control-Allow-Origin': '*',
-};
+// // Set up the request headers, including the API key and the CORS header
+// const headers = {
+//   Authorization: `Bearer ANqL8DWNmjbGyOciXVpmjB6x3hLHLW8EyzUS8z5hSZSSHjS2E7vzhlJsXQYVqiSfSQJzfW3ZeFP5SCV-jggrnLwP7xkrX47lHHn1lGEK9OdEYNKIT7_ujom13aYKZHYx`,
+//   'Access-Control-Allow-Origin': '*',
+//   accept: 'application/json',
+// };
 
-// Send the API request and get the response
-axios
-  .get(url, { headers, params })
-  .then((response) => {
-    // Handle the response data
-    console.log(response.data);
-  })
-  .catch((error) => {
-    // Handle any errors
-    console.error(error);
+// // Send the API request and get the response
+// axios
+//   .get(url, { headers, params })
+//   .then((response) => {
+//     // Handle the response data
+//     console.log(response.data);
+//   })
+//   .catch((error) => {
+//     // Handle any errors
+//     console.error(error);
+//   });
+//   }
+
+
+async searchBusinesses(term: 'innout',location: 'irvine'): Promise<YelpBusiness[]> {
+  const response = await axios.get(`${CORS_PROXY}${BASE_URL}${SEARCH_ENDPOINT}`, {
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    params: {
+      term,
+      location,
+    },
   });
-  }
+  return response.data.businesses.map((business: any) => ({
+    name: business.name,
+    rating: business.rating,
+    phone: business.phone,
+  }));
+}
 
 
 
