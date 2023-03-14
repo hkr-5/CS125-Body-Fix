711b223ed62b07a4d92c09a5b47d4b1cd83e7f86
Panzerchong <panzerchong@gmail.com>
2023-03-13 21:41:43 -0700

    mo

--- app/tab3/tab3.page.ts
+++ app/tab3/tab3.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import axios from 'axios';
 
 @Component({
   selector: 'app-tab3',
@@ -8,8 +9,14 @@ import { AlertController } from '@ionic/angular';
 })
 
 export class Tab3Page {
+
+  searchItem:string;
+  restaurants;
   
-  constructor(private alertController: AlertController) {}
+  constructor(private alertController: AlertController) {
+    this.searchItem="";
+    this.restaurants=[];
+  }
   async presentAlert() {
     const distance = await this.alertController.create({
       header: 'Select different location',
@@ -84,48 +91,37 @@ export class Tab3Page {
     await star.present();
   }
   
-  getFile(){
-    const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer 5icQU-p04RYAvXO0IENLAB2UnnN7DbuRVHtvGYhNWmi0gklKJVeoPG0-Ze1pnAUpSKYXkgANkZn34Uek_kgfZ-3XjNEZI7rY4B3xP88YrDqrzTibfqQaK23nkekMZHYx',
-    }
-      };
-      fetch('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=irvine&term=innout&sort_by=best_match&limit=20', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+  getFile(item:string){
+  const url = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search';
+  const params = {
+    term: item,
+    location: 'irvine',
+  };
+
+  const headers = {
+    Authorization: `Bearer ANqL8DWNmjbGyOciXVpmjB6x3hLHLW8EyzUS8z5hSZSSHjS2E7vzhlJsXQYVqiSfSQJzfW3ZeFP5SCV-jggrnLwP7xkrX47lHHn1lGEK9OdEYNKIT7_ujom13aYKZHYx`,
+    accept: 'application/json',
+  };
+
+  axios
+    .get(url, { headers, params }).then((response) => {
+      this.restaurants=response.data;
+      
+
+
+      console.log(response.data);})
+    .catch((error) => {console.error(error);
+    });
   }
 
-//   getFile(){
-//     const url = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search';
 
-// // Set up the request parameters, such as the search term and location
-// const params = {
-//   term: 'innout',
-//   location: 'irvine',
-// };
 
-// // Set up the request headers, including the API key and the CORS header
-// const headers = {
-//   Authorization: `Bearer ANqL8DWNmjbGyOciXVpmjB6x3hLHLW8EyzUS8z5hSZSSHjS2E7vzhlJsXQYVqiSfSQJzfW3ZeFP5SCV-jggrnLwP7xkrX47lHHn1lGEK9OdEYNKIT7_ujom13aYKZHYx`,
-//   'Access-Control-Allow-Origin': '*',
-//   accept: 'application/json',
-// };
+//   ionChange(event) {
 
-// // Send the API request and get the response
-// axios
-//   .get(url, { headers, params })
-//   .then((response) => {
-//     // Handle the response data
-//     console.log(response.data);
-//   })
-//   .catch((error) => {
-//     // Handle any errors
-//     console.error(error);
-//   });
-//   }
+//     this.getFile(event.detail.value);
+
+//     console.log(this.getFile(event.detail.value));
+// }
 
 
 }
