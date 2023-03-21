9988a43e6c974abab0d7efc1e267818fd4c957f7
Hikaru Y <83538988+hkr-5@users.noreply.github.com>
2023-03-21 13:09:35 -0700

    Delete app directory

--- app/data/log-data.ts
+++
@@ -1,9 +0,0 @@
-export class LogData {
-  timestamp: number;
-  weight: number;
-
-  constructor(timestamp: number, weight: number) {
-    this.timestamp = timestamp;
-    this.weight = weight;
-  }
-}
